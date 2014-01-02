1289459af43c622bd75a367378c7ba7032af1436
Robear Geleta <rgeleta@colgate.edu>
2014-01-02 14:15:30 -0500

    string reversal via recurrsion

---
+++ fun_problems.py
@@ -0,0 +1,11 @@
+#/bin/usr/python
+
+
+#string reversal via recursion 
+def rev_s( input):
+    if len(input)==1:
+        return input
+    else:
+        return input[-1]+(rev_s(input[:len(input)-1]))
+
+print rev_s("abcd")
