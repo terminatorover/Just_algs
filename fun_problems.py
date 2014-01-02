2e9317831aea8a2ab41fc56ec020fe3265369ca6
Robear Geleta <rgeleta@colgate.edu>
2014-01-02 14:38:05 -0500

    palindrome checker

--- fun_problems.py
+++ fun_problems.py
@@ -8,4 +8,24 @@ def rev_s( input):
     else:
         return input[-1]+(rev_s(input[:len(input)-1]))
 
-print rev_s("abcde")
+print rev_s("hello")
+
+
+#palindrome checker via recursion 
+
+def p_check( input):
+    pure = input.split()
+    input = ''.join(pure)
+    if len(input) <=1:
+        return True 
+    else:
+        first = input[0].upper()
+        last = input[-1].upper()
+        if ( first != last):
+            return False
+        else:
+            return p_check(input[1:len(input)-1])
+
+print p_check("aibohphobia")            
+print p_check("Live not on evil")
+
