97c5f66b8fce1c451cd163348b5026f7aac30ca1
Robear Geleta <rgeleta@colgate.edu>
2014-01-08 17:15:55 -0500

    clean implementaion of mergesort

--- sort.py
+++ sort.py
@@ -40,3 +40,36 @@ li = [4,-1,0,10,7,6]
 print li
 bubble_sort(li)
 print li
+
+#efficient merge sort
+
+def merge_step(list1,list2):
+    l_1 = len(list1)
+    l_2 = len(list2)
+    ind_1 = ind_2 = 0
+    result = [] 
+    while( ( ind_1 < l_1) and (ind_2 < l_2 )):
+        if (list1[ind_1 ] <= list2[ind_2]):
+            result.append(list1[ind_1])
+            ind_1 +=1 
+        else:
+            result.append(list1[ind_2])   
+            ind_2 +=1 
+
+    result.extend(list1[ind_1:])
+    result.extend(list1[ind_2:])
+    return result
+
+def merge(input_list):
+    if len(input_list ) ==1:
+        return input_list
+    else:
+        mid = len(input_list)/2
+        first_half = input_list[:mid]
+        second_half = input_list[mid:]
+        return merge_step(first_half,second_half)
+
+li = [3,4,-1,0,10,7,6,100]
+print li
+merge(li)
+print li
